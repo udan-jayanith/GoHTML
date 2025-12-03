29ec597228e6c92f066dff388799df3b1b91dab0
Udan Jayanith <j.udanjayanith@gmail.com>
2025-12-03 19:21:17 +0530

    Add tests to Encode function

--- serializer_test.go
+++ serializer_test.go
@@ -1,9 +1,11 @@
 package GoHtml_test
 
 import (
+	"errors"
+	"os"
 	"strings"
 	"testing"
-	"os"
+
 	GoHtml "github.com/udan-jayanith/GoHTML"
 )
 
@@ -23,15 +25,33 @@ func TestEncode1(t *testing.T) {
 	//It's hard compare exacted output. Because strings, prettier formats html code. htmlFormatter and prettier add extra stuffs to the html codes like dash in void tags. Exacted output is in the ./test-files/2.html.
 }
 
+type fakeWriter struct {
+}
+
+func (_ fakeWriter) Write(p []byte) (n int, err error) {
+	return 0, errors.New("Error")
+}
+
 func TestEncode2(t *testing.T) {
 	file, err := os.Open("./test-files/1.html")
 	if err != nil {
 		t.Fatal("1.html does not exists.")
 	}
 	defer file.Close()
-	
-	node, _  := GoHtml.Decode(file)
+
+	node, _ := GoHtml.Decode(file)
 	var builder strings.Builder
-	GoHtml.Encode(&builder, node)
+	err = GoHtml.Encode(&builder, node)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	//It's hard compare exacted output. Because strings, prettier formats html code. htmlFormatter and prettier add extra stuffs to the html codes like dash in void tags. Exacted output is in the ./test-files/2.html.
+}
+
+func TestEncode3(t *testing.T) {
+	err := GoHtml.Encode(&fakeWriter{}, GoHtml.CreateTextNode(""))
+	if err == nil {
+		t.Fatal("Expected a error but got no error")
+	}
 	//It's hard compare exacted output. Because strings, prettier formats html code. htmlFormatter and prettier add extra stuffs to the html codes like dash in void tags. Exacted output is in the ./test-files/2.html.
 }
